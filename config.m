fcacf9aa9e064526f8544ff6269fd993ce5d259f
antoine <roygobeil.antoine@gmail.com>
2012-07-16 15:45:59 -0400

    Put an aboslute path in the config as a relative one might break things

--- config.m
+++ config.m
@@ -13,6 +13,6 @@ s.UserFunctions = {'Fit(channel,name)'};
 %%%%
 %   CACHING
 %%%%
-s.Caching.Enabled = false;
-s.Caching.Folder = '.'; % either an absolute path or relative path from parent directory of the file
+s.Caching.Enabled = true;
+s.Caching.Folder = '/tmp'; % either an absolute path or relative path from parent directory of the file
 end
