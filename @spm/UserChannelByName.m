2e106b4b2155d317bd1e4e934305dfe37f41ae62
Antoine Roy-Gobeil <roygobeil.antoine@gmail.com>
2013-04-05 00:03:31 -0400

    UserChannelByName throws error if empty

--- @spm/UserChannelByName.m
+++ @spm/UserChannelByName.m
@@ -1,4 +1,6 @@
 function ch=UserChannelByName(sf,name)
 %ChannelByName returns userchannel based on its name
     ch = findobj(sf.UserChannel,'Name',name);
+    if isempty(ch)
+        throw(MException('SPM:Not_found',['No UserChannels named' name]));
 end
