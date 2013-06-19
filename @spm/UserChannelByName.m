1343c808e3f2545c2f9be92856b27744b2f4f9f0
Antoine Roy-Gobeil <roygobeil.antoine@gmail.com>
2013-06-18 21:31:26 -0400

    Fix whitespace in error message

--- @spm/UserChannelByName.m
+++ @spm/UserChannelByName.m
@@ -2,5 +2,5 @@ function ch=UserChannelByName(sf,name)
 %ChannelByName returns userchannel based on its name
     ch = findobj(sf.UserChannel,'Name',name);
     if isempty(ch)
-        throw(MException('SPM:Not_found',['No UserChannels named' name]));
+        throw(MException('SPM:Not_found',['No UserChannels named ' name]));
 end
