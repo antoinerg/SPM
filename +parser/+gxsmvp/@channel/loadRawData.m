08857a79ce7c779e9599e0dffc426aa1a80cce66
Antoine Roy-Gobeil <roygobeil.antoine@gmail.com>
2014-10-31 13:43:47 -0400

    gxsmvp now properly read data point and passes tests

--- +parser/+gxsmvp/@channel/loadRawData.m
+++ +parser/+gxsmvp/@channel/loadRawData.m
@@ -10,13 +10,20 @@ i=1;
 
 pos=vpch.pos;
 rawData=zeros(vp.NPoints,1);
+
+myHeader = '#C Index';
+while(~eof)
+    line=fgets(fid);
+    if ~(isempty(strfind(line,myHeader)));
+        break;
+    end
+end
+
 while(~eof)
 	line=fgets(fid);
-	if ~(line(1) == '#')
-	  num2str(line);
-	  rawData(i)=num2str(pos);
-	  i=i+1;
-	end
+    myNums=str2num(line);
+    rawData(i)=myNums(pos);
+	i=i+1;
 	if (i == vp.NPoints), break; end % Stop reading after NPoints
 	if( (-1)==line ), eof  = 1; end % End of file condition
 end
