e97d4a165c33b37c52dc947f0f7ece8e1d7c1505
Antoine Roy-Gobeil <roygobeil.antoine@gmail.com>
2015-11-24 15:39:56 -0500

    Update convertRawData.m

--- +parser/+nanonisspectrum/@channel/convertRawData.m
+++ +parser/+nanonisspectrum/@channel/convertRawData.m
@@ -1,7 +1,7 @@
 function convertRawData(nsch)
 % Backward data is saved in an misleading way. We need to flip the data for backward scan to make sure they are associated with the right bias value
 if strcmp(nsch.Direction,'Backward')
-	nsch.Data = fliplr(nsch.rawData)
+	nsch.Data = flipud(nsch.rawData)
 else
 	nsch.Data = nsch.rawData;
 end
