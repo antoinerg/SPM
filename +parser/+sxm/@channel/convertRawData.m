e49dc138c70e8038a02a15a29f5ac011b0e7ab8a
antoinerg <roygobeil.antoine@gmail.com>
2015-12-01 18:43:56 -0500

    SXM parser correctly flip data according to scan direction

--- +parser/+sxm/@channel/convertRawData.m
+++ +parser/+sxm/@channel/convertRawData.m
@@ -1,8 +1,12 @@
 function convertRawData(nsch)
+nsch.Data = nsch.rawData;
+
+% Following Nanonis convention
 if strcmpi(nsch.Direction,'Backward')
-    nsch.Data = fliplr(nsch.rawData);
-else
-    nsch.Data = nsch.rawData;
+    nsch.Data = fliplr(nsch.Data);
+end
+if strcmpi(nsch.spm.Header.scan_dir,'up')
+	nsch.Data = flipud(nsch.Data)
 end
 end
 
